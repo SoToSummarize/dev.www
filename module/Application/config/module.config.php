8883c7836b6a8b3a41e087eafb15f82e226cb5b5
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-08 14:13:34 -0800

    Rename IndexController.php to just Index.php

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,12 +1,7 @@
 <?php
-/**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 namespace Application;
 
+use Application\Controller\Index as IndexController;
 use Zend\Router\Http\Literal;
 use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
@@ -19,7 +14,7 @@ return [
                 'options' => [
                     'route'    => '/',
                     'defaults' => [
-                        'controller' => Controller\IndexController::class,
+                        'controller' => IndexController::class,
                         'action'     => 'index',
                     ],
                 ],
@@ -38,7 +33,7 @@ return [
     ],
     'controllers' => [
         'factories' => [
-            Controller\IndexController::class => InvokableFactory::class,
+            IndexController::class => InvokableFactory::class,
         ],
     ],
     'view_manager' => [
