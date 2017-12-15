7a118f5e8fe8038313daef7d09e1b54cedc1a9e6
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-15 12:04:42 -0800

    Add leo-galleguillos/summary package to repo

--- config/modules.config.php
+++ config/modules.config.php
@@ -1,12 +1,9 @@
 <?php
-/**
- * List of enabled modules for this application.
- *
- * This should be an array of module namespaces used in the application.
- */
+
 return [
     'LeoGalleguillos\Flash',
     'LeoGalleguillos\String',
+    'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
     'Application',
