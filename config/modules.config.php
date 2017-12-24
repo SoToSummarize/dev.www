4fe25c56e08eb6df39bb2ee46f9f491c2ee0efb8
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-24 11:40:39 -0800

    Add LeoGalleguillos\Twitter to list of modules

--- config/modules.config.php
+++ config/modules.config.php
@@ -5,6 +5,7 @@ return [
     'LeoGalleguillos\Flash',
     'LeoGalleguillos\String',
     'LeoGalleguillos\Summary',
+    'LeoGalleguillos\Twitter',
     'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
