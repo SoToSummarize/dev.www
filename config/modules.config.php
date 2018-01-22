5d0744ba9dc963cd4685f056e4f86806eb784b54
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-22 08:12:07 -0800

    Run composer update and add modules to config

--- config/modules.config.php
+++ config/modules.config.php
@@ -3,9 +3,11 @@
 return [
     'LeoGalleguillos\Facebook',
     'LeoGalleguillos\Flash',
+    'LeoGalleguillos\Html',
     'LeoGalleguillos\String',
     'LeoGalleguillos\Summary',
     'LeoGalleguillos\Twitter',
+    'LeoGalleguillos\Website',
     'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
